550d1c9aa312f075db84cf13a2884cc4364d733b
EveRuby <evgeniyarubin@gmail.com>
2021-05-28 10:28:50 -0400

    capture user inpu

--- game.py
+++ game.py
@@ -1,1 +1,10 @@
-print ("rock, paper, scissors, shoot!")
+
+print("rock, paper, scissors, shoot!")
+
+print (10)
+
+print (10, 20, "test")
+
+user_choice = input ("Please choose one of 'rock', 'paper', scissors':")
+
+print ("You selected", user_choice)
