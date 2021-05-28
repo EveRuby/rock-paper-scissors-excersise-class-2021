3feb5f4b054cc7647de1d078b744542bd1fb37e6
EveRuby <evgeniyarubin@gmail.com>
2021-05-28 10:53:16 -0400

    Random Variable generator

--- game.py
+++ game.py
@@ -1,10 +1,28 @@
 
 print("rock, paper, scissors, shoot!")
 
-print (10)
+import random
+#print (10)
+#print (10, 20, "test")
 
-print (10, 20, "test")
+user_choice = input ("PLEASE CHOOSE ONE OF THE FOLLOWING: 'rock', 'paper', 'scissors'")
 
-user_choice = input ("Please choose one of 'rock', 'paper', scissors':")
+print ("YOU SELECTED", user_choice)
 
-print ("You selected", user_choice)
+#validate that user choice is valid, if not valid present end of program message
+#Operators:
+#and
+#or
+
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print("VALID. KEEP GOING")
+else:
+    print("OOPS, INVALID INPUT. PLEASE TRY AGAIN")
+    exit()
+
+valid_options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(valid_options)
+print("COMPUTER CHOICE:", computer_choice)
+
+print("THIS IS THE END OF OUR GAME. PLEASE TRY AGAIN")
+
