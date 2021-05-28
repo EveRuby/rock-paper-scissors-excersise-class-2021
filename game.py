b4e2720e59eadd4362a659735f03825fdf473666
EveRuby <evgeniyarubin@gmail.com>
2021-05-28 11:08:58 -0400

    determine a winner

--- game.py
+++ game.py
@@ -24,5 +24,29 @@ valid_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(valid_options)
 print("COMPUTER CHOICE:", computer_choice)
 
+#adopted from JanZielonka in Slack. Thanks so much to Jan for saving me so much time.
+
+if user_choice == "rock":
+    if computer_choice == "rock":
+        print("IT'S A TIE")
+    elif computer_choice == "paper":
+        print("PAPER BEATS ROCK, THE COMPUTER WON...")
+    elif computer_choice == "scissors":
+        print("ROCK BEATS SCISSORS! CONGRATS!")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("PAPER BEATS ROCK. YOU WON! CONGRATS!")
+    elif computer_choice == "paper":
+        print("IT'S A TIE")
+    elif computer_choice == "scissors":
+        print("SCISSORS CUT PAPER. THE COMPUTER WON...")
+elif user_choice == "scissors":
+    if computer_choice == "rock":
+        print("ROCK BEATS SCISSORS, THE COMPUTER WON...")
+    elif computer_choice == "paper":
+        print("SCISSORS CUT PAPER. YOU WON! CONGRATS!")
+    elif computer_choice == "scissors":
+        print("IT'S A TIE")
+
 print("THIS IS THE END OF OUR GAME. PLEASE TRY AGAIN")
 
